d1ea0e5de54517dd8ea97d4836f176b7861a542e
Moudy Rasmy <bobmoudy1@gmail.com>
2025-06-14 13:53:27 +0300

    Refactor Swagger setup for modularity and maintainability

--- Backend/Eshop.Presentation/Program.cs
+++ Backend/Eshop.Presentation/Program.cs
@@ -94,48 +94,8 @@ namespace Eshop.Presentation
 
 
 
-            /*-----------------------------Swagger PArt-----------------------------*/
-            #region Swagger REgion
-
-            builder.Services.AddSwaggerGen(swagger =>
-            {
-                swagger.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "ASP.NET 5 Web API",
-                    Description = "Eshop E-Commerce"
-                });
-
-                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
-                });
-
-                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        }
-                    },
-                    new string[] {}
-                }
-            });
-            });
-
-            #endregion
-            //----------------------------------------------------------
-
-
+            
+            builder.Services.AddSwaggerDocumentation();
             var app = builder.Build();
 
 
